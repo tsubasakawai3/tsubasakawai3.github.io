9c805727bbffb997b1728450f1b086b2b4cdc1a8
tsubasakawai3 <tsubasakawai81@gmail.com>
2025-08-03 05:59:15 +0000

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-react-project/',
+  base: '/',
   plugins: [react()],
   // for forwarding container ports
   server: {
